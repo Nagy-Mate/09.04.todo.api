19093c98f6f48f935422c3dd7d17aed5ca0c3b4a
Nagy Máté <morsakk.mate@gmail.com>
2025-09-11 08:58:12 +0200

    új módszerrel update

--- Solution/Solution.Services/TodoService.cs
+++ Solution/Solution.Services/TodoService.cs
@@ -22,24 +22,52 @@ public class TodoService(TodoDbContext db) : ITodoService
     }
     public async Task UpdateAsync(Todo entity)
     {
-        var toUpdate = db.Todos.FirstOrDefault(todo => todo.Id == entity.Id);
+        //régi mód/
+        /*var toUpdate = db.Todos.FirstOrDefault(todo => todo.Id == entity.Id);
         if (toUpdate != null)
         {
             toUpdate.Title = entity.Title;
             toUpdate.Description = entity.Description;
             toUpdate.DaeadLine= entity.DaeadLine;
             toUpdate.IsReady = entity.IsReady;
-            await db.SaveChangesAsync();    
+
+            await db.SaveChangesAsync();
         }
+        else
+        {
+            throw new Exception("Enitty not found");
+        }*/
+
+        //új módszer
+        await db.Todos.Where(t => t.Id == entity.Id).ExecuteUpdateAsync(
+            setters => 
+                setters.SetProperty(e => e.Description, entity.Description)
+                .SetProperty(e => e.Title, entity.Title)
+                .SetProperty(e => e.DaeadLine, entity.DaeadLine)
+                .SetProperty(e => e.IsReady, entity.IsReady)
+            );
+        await db.SaveChangesAsync();
+
     }
 
     public async Task ReadyAsync(int id)
     {
+        /*régi módszer
         var toReady = db.Todos.FirstOrDefault(todo => todo.Id == id);
         if (toReady != null)
         {
             toReady.IsReady = true;
+
+            await db.SaveChangesAsync();
         }
+        else
+        {
+            throw new Exception("Enitty not found");
+        }*/
+        //új mód
+        await db.Todos.Where(t => t.Id == id).ExecuteUpdateAsync(
+            setters => setters.SetProperty(e => e.IsReady, true));
         await db.SaveChangesAsync();
+
     }
 }
